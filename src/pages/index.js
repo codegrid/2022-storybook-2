7eae1332c6b555401338c88d2a6ab139d5b89fcf
tomof <fffjt007@gmail.com>
2022-07-14 14:21:15 +0900

    Buttonコンポーネントとログインフォームを追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Button } from '../components/Button'
 
 export default function Home() {
   return (
@@ -8,7 +9,28 @@ export default function Home() {
         <title>Storybook Sample</title>
         <meta name="description" content="Storybook Sample" />
       </Head>
-      <main className={styles.main}>Storybook Sample</main>
+      <main className={styles.main}>
+        <form className={styles.form}>
+          <span className={styles.title}>My App</span>
+          <div className={styles.controlGroup}>
+            <label htmlFor="email" className={styles.label}>
+              メールアドレス
+            </label>
+            <input id="email" type="text" className={styles.formControl} />
+          </div>
+          <div className={styles.controlGroup}>
+            <label htmlFor="password" className={styles.label}>
+              パスワード
+            </label>
+            <input id="password" type="password" className={styles.formControl} />
+          </div>
+          <div className={styles.buttonGroup}>
+            <Button type="submit" fullWidth>
+              ログイン
+            </Button>
+          </div>
+        </form>
+      </main>
     </div>
   )
 }
